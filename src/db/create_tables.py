55701157976fad6e92c441ea72066ec55927b456
macalistervadim <macalistervadim@yandex.ru>
2025-04-22 10:45:52 +0600

    feat(pytest): add pytest in proj / feat(ci/cd): add new pipelines

--- src/db/create_tables.py
+++
@@ -1,7 +0,0 @@
-from src.db.session import engine
-from src.models.post import Base
-
-
-async def create_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
---
+++ tests/test_services/test_post_services.py
@@ -0,0 +1,27 @@
+from unittest.mock import AsyncMock
+
+import pytest
+
+from src.schemas.post import PostCreate
+from src.services.post import PostServices
+
+
+@pytest.mark.asyncio
+async def test_add_post():
+    mock_repo = AsyncMock()
+    mock_repo.add_one.return_value = 1
+
+    service = PostServices(mock_repo)
+
+    post = PostCreate(title="Test title", content="Test content")
+
+    result = await service.add_post(post)
+
+    assert result == 1
+
+    mock_repo.add_one.assert_called_once_with(
+        {
+            "title": "Test title",
+            "content": "Test content",
+        },
+    )
