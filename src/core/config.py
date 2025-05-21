41397bdb9cacbd7eb55915a1c5b656a848f2002c
macalistervadim <macalistervadim@yandex.ru>
2025-05-21 12:59:08 +0600

    fix(pydantic-config): fix cfg

--- src/core/config.py
+++ src/core/config.py
@@ -5,11 +5,6 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    POSTGRES_DB: str
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_HOST: str = "database"
-    POSTGRES_PORT: int = 5432
 
     class Config:
         env_file = os.environ.get("ENV_FILE", ".env.local")
